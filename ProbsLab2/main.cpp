d52c0b6180c21a3f03709baed2efef80e60c3f62
Maria del Mar <maria.arbelaezs@udea.edu.co>
2022-01-22 12:53:05 -0500

    prob2

--- ProbsLab2/main.cpp
+++ ProbsLab2/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+/*//Ej2 Letras mayúsculas
+#include <cstdlib>
+#include <ctime>
+int main(){
+    int num=200;
+    char abra[num]; //65-90 25 letras
+    int veces[25]={0};
+    srand(time(0));
+    for (int i=0;i<num;i++) {
+        char letra=rand()%25+66;
+        abra[i]=letra;
+        veces[letra-65]=veces[letra-65]+1;
+    }
+    cout.flush();
+    for (int i=0;i<num;i++){
+            cout<<abra[i];
+        }
+    cout<<endl;
+    for (int i=0;i<25;i++){
+        if (veces[i]>0){
+            cout<<char(i+65)<<':'<<veces[i]<<endl;
+        }
+    }
+return 0;
+}*/
+/Ej4
+int convert(char a){
+
 }
